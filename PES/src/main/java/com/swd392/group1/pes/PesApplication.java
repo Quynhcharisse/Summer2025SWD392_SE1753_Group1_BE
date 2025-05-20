3deaa4e53131f940f5d18b2ffc1cf049bf5d6ebc
Pham Van Nhu Quynh <vannhuquynhp@gmail.com>
2025-05-20 21:21:27 +0700

    first commit

---
+++ PES/src/main/java/com/swd392/group1/pes/PesApplication.java
@@ -0,0 +1,13 @@
+package com.swd392.group1.pes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PesApplication.class, args);
+    }
+
+}
---
+++ PES/src/test/java/com/swd392/group1/pes/PesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swd392.group1.pes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
