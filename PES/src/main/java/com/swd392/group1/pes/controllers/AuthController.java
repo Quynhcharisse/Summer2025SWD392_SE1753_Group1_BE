2d0ce8337f71748320fd3607d0ab01c85ce20195
Quynhcharisse <vannhuquynhp@gmail.com>
2025-05-28 11:08:18 +0700

    Change package import into single import

--- PES/src/main/java/com/swd392/group1/pes/controllers/AuthController.java
+++ PES/src/main/java/com/swd392/group1/pes/controllers/AuthController.java
@@ -9,7 +9,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
